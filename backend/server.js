da790f5dfb9d15de6c8e3ed9fe7a5e2a61e0bbbe
Mickyvirus123 <120229172+Mickyvirus123@users.noreply.github.com>
2026-01-25 21:13:17 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Initialize Express
 const app = express();
-const PORT = process.env.PORT || 5001; // Change from 5000 to 5001
+const PORT = process.env.PORT || 5000; // Change from 5000 to 5001
 
 // Connect to Database
 connectDB();
